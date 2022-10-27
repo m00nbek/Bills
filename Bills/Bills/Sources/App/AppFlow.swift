52f097f4e2c8d03ed6a51a09bf08e3e25248b9f3
m00nbek <melikulov990@gmail.com>
2022-10-27 08:07:52 +0500

    `AppFlow` setup

---
+++ Bills/Bills/Sources/App/AppFlow.swift
@@ -0,0 +1,23 @@
+//
+//  AppFlow.swift
+//  Bills
+//
+//  Created by m00nbek Melikulov on 10/27/22.
+//
+
+import UIKit
+
+final public class AppFlow {
+    let navigationController: UINavigationController
+    
+    var homeVC = UIViewController()
+    
+    public init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        navigationController.setViewControllers([homeVC], animated: false)
+    }
+}
+
---
+++ Bills/Bills/Sources/App/RootController.swift
@@ -0,0 +1,18 @@
+//
+//  RootController.swift
+//  Bills
+//
+//  Created by m00nbek Melikulov on 10/27/22.
+//
+
+import UIKit
+
+class RootConasdatroller: UINavigationController {
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationBar.isHidden = true
+    }
+}
+
