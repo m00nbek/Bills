d278c0f4eb6b431910de248f04bd16dd8c9b742b
m00nbek <melikulov990@gmail.com>
2022-10-27 07:42:37 +0500

    initial xcode project

---
+++ Bills/Bills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bills
+//
+//  Created by m00nbek Melikulov on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
