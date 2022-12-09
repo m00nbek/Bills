78e9fbd99016bfb1276a21bda97609e81feb01e1
m00nbek <melikulov990@gmail.com>
2022-12-09 20:55:19 +0500

    Save command requests cache deletion

--- Feed/FeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ Feed/FeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import Feed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -24,5 +35,20 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), title: "any", timestamp: Date.now, cost: 0, currency: .USD)
     }
 }
