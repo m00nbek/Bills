74345961d30e10bf14ef620e7a3be5c3b84188ee
m00nbek <melikulov990@gmail.com>
2022-12-24 19:42:26 +0500

    checking error code & domain

--- Feed/FeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ Feed/FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -43,10 +43,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let requestError = anyNSError()
         
-        let receivedError = resultErrorFor(data: nil, response: nil, error: requestError)
+        let receivedError = resultErrorFor(data: nil, response: nil, error: requestError) as? NSError
         
-        // comparing the domains for now... because of userInfo
-        XCTAssertEqual((receivedError as NSError?)?.domain, requestError.domain)
+        XCTAssertEqual(receivedError?.domain, requestError.domain)
+        XCTAssertEqual(receivedError?.code, requestError.code)
     }
     
     func test_getFromURL_failsOnAllInvalidRepresentationCases() {
