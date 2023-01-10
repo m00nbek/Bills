0633b4e9838c54876a4333ff0d7ede1956fbb145
m00nbek <melikulov990@gmail.com>
2023-01-10 18:45:34 +0500

    Extract `FailableDeleteFeedStoreSpecs` checks into reusable assertions

---
+++ Feed/FeedTests/Feed Cache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  FeedTests
+//
+//  Created by m00nbek Melikulov on 1/10/23.
+//
+
+import XCTest
+import Feed
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
