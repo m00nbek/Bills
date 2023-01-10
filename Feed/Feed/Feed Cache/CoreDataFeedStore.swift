79e97d1c023f613ac82a92be0f8709f81f9293ae
m00nbek <melikulov990@gmail.com>
2023-01-10 19:38:22 +0500

    Add private background context to perform store operations

--- Feed/Feed/Feed Cache/CoreDataFeedStore.swift
+++ Feed/Feed/Feed Cache/CoreDataFeedStore.swift
@@ -9,11 +9,13 @@ import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
-
-     public init(bundle: Bundle = .main) throws {
-         container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
-     }
-
+    private let context: NSManagedObjectContext
+    
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
@@ -34,17 +36,17 @@ private extension NSPersistentContainer {
         case modelNotFound
         case failedToLoadPersistentStores(Swift.Error)
     }
-
+    
     static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
             throw LoadingError.modelNotFound
         }
-
+        
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
         var loadError: Swift.Error?
         container.loadPersistentStores { loadError = $1 }
         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
+        
         return container
     }
 }
