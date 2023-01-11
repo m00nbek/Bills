e2b07ed87ac63ca2e805fe7bad66d5dfe13a1a0b
m00nbek <melikulov990@gmail.com>
2023-01-11 18:46:46 +0500

    Add `FeedExpenseCell` with storyboard connections

---
+++ Prototype/Prototype/FeedExpenseCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedExpenseCell.swift
+//  Prototype
+//
+//  Created by m00nbek Melikulov on 1/11/23.
+//
+
+import UIKit
+
+final class FeedExpenseCell: UITableViewCell {
+    @IBOutlet private(set) var title: UILabel!
+    @IBOutlet private(set) var cost: UILabel!
+    @IBOutlet private(set) var date: UILabel!
+}
+
