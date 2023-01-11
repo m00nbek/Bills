854cb97247bcefd985cb3632e600bb8e950e16da
m00nbek <melikulov990@gmail.com>
2023-01-11 08:52:10 +0500

    Add `FeedCacheIntegrationTests` target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ Feed/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCacheIntegrationTests.swift
+//  FeedCacheIntegrationTests
+//
+//  Created by m00nbek Melikulov on 1/11/23.
+//
+
+import XCTest
+
+final class FeedCacheIntegrationTests: XCTestCase {
+
+}
