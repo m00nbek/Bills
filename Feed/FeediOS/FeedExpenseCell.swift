d7c94e3e575316ddea44b91019cbf2f5111a9834
m00nbek <melikulov990@gmail.com>
2023-01-12 14:55:25 +0500

    Render loaded feed on successful load completion

---
+++ Feed/FeediOS/FeedExpenseCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedExpenseCell.swift
+//  FeediOS
+//
+//  Created by m00nbek Melikulov on 1/12/23.
+//
+
+import UIKit
+
+ public final class FeedExpenseCell: UITableViewCell {
+     public let expenseTitleLabel = UILabel()
+     public let costLabel = UILabel()
+     public let dateLabel = UILabel()
+ }
--- Feed/FeediOS/FeedViewController.swift
+++ Feed/FeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import Feed
 
 final public class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
+    private var tableModel = [FeedExpense]()
     
     public convenience init(loader: FeedLoader) {
         self.init()
@@ -26,8 +27,41 @@ final public class FeedViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
             self?.refreshControl?.endRefreshing()
         }
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedExpenseCell()
+        cell.expenseTitleLabel.text = cellModel.title
+        cell.costLabel.text = "\(cellModel.cost)"
+        cell.dateLabel.text = cellModel.timestamp.formatted()
+        return cell
+    }
+}
+
+private extension Date {
+    func formatted() -> String {
+        let days = days(self)
+        switch days {
+        case 0:
+            return "Today"
+        case 1:
+            return "Yesterday"
+        default:
+            return "\(days) days ago"
+        }
+    }
+    
+    private func days(_ start: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: start, to: Date()).day!
     }
 }
