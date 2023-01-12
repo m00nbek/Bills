c72a275c8732dae48cf2b1a84749cabb4e9e9fb0
m00nbek <melikulov990@gmail.com>
2023-01-12 10:54:15 +0500

    Move `FeedViewController` to production target

---
+++ Feed/FeediOS/FeedViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedViewController.swift
+//  FeediOS
+//
+//  Created by m00nbek Melikulov on 1/12/23.
+//
+
+import UIKit
+import Feed
+
+final public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
+        refreshControl?.beginRefreshing()
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
