61732a0288773f6073161a7f831711c9d298bee6
m00nbek <melikulov990@gmail.com>
2023-01-15 11:22:06 +0500

    Extract custom assertions to a separate file

--- Feed/FeediOSTests/Controllers/FeedViewControllerTests.swift
+++ Feed/FeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -81,31 +81,7 @@ final class FeedViewControllerTests: XCTestCase {
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
-    }
-    
-    private func assertThat(_ sut: FeedViewController, isRendering feed: [FeedExpense], file: StaticString = #file, line: UInt = #line) {
-        guard sut.numberOfRenderedFeedExpenseViews() == feed.count else {
-            return XCTFail("Expected \(feed.count) expenses, got \(sut.numberOfRenderedFeedExpenseViews()) instead.", file: file, line: line)
-        }
-
-        feed.enumerated().forEach { index, expense in
-            assertThat(sut, hasViewConfiguredFor: expense, at: index, file: file, line: line)
-        }
-    }
-    
-    private func assertThat(_ sut: FeedViewController, hasViewConfiguredFor expense: FeedExpense, at index: Int, file: StaticString = #file, line: UInt = #line) {
-        let view = sut.feedExpenseView(at: index)
-
-        guard let cell = view as? FeedExpenseCell else {
-            return XCTFail("Expected \(FeedExpenseCell.self) instance, got \(String(describing: view)) instead", file: file, line: line)
-        }
-
-        XCTAssertEqual(cell.titleText, expense.title, "Expected title text to be \(expense.title) for expense view at index (\(index))", file: file, line: line)
-
-        XCTAssertEqual(cell.costTextAsFloat, expense.cost, "Expected cost text to be \(expense.cost) for expense  view at index (\(index))", file: file, line: line)
-
-        XCTAssertEqual(cell.dateText, expense.timestamp.formatted(), "Expected date text to be \(expense.timestamp.formatted()) for expense view at index (\(index)", file: file, line: line)
-    }
+    }    
     
     private func makeExpense(title: String, timestamp: Date, cost: Float) -> FeedExpense {
         return FeedExpense(id: UUID(), title: title, timestamp: timestamp, cost: cost, currency: .USD)
