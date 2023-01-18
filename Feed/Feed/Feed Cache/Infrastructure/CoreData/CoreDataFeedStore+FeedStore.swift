a955dacdfd28f6f42e8e7c795b4bd95ca1016097
m00nbek <melikulov990@gmail.com>
2023-01-18 16:26:35 +0500

    make `CoreDataFeedStore` conform to `FeedStore`

---
+++ Feed/Feed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataFeedStore+FeedStore.swift
+//  Feed
+//
+//  Created by m00nbek Melikulov on 1/18/23.
+//
+
+
+import CoreData
+
+extension CoreDataFeedStore: FeedStore {
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map {
+                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp!)
+                }
+            })
+        }
+    }
+    
+    public func insert(_ feed: [LocalFeedExpense], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedExpense.expenses(from: feed, in: context)
+                try context.save()
+            })
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+            })
+        }
+    }
+    
+}
