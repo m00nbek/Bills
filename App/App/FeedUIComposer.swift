afef419a32b778be848c61e37f806780640c262a
m00nbek <melikulov990@gmail.com>
2023-01-19 16:10:16 +0500

    Move `dispatchOnMainQueue` operator to the point of use (before subscription with `sink`)

--- App/App/FeedUIComposer.swift
+++ App/App/FeedUIComposer.swift
@@ -13,7 +13,7 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: { feedLoader().dispatchOnMainQueue() })
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         
         let feedController = makeFeedViewController(
             delegate: presentationAdapter,
