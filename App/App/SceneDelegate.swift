5cc76555f982eaf73551ec74dbd2f2ae7af2dbbb
m00nbek <melikulov990@gmail.com>
2023-01-19 10:02:45 +0500

    Remove unused debugging code that was previously used during UI tests

--- App/App/SceneDelegate.swift
+++ App/App/SceneDelegate.swift
@@ -13,16 +13,15 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
-    let localStoreURL = NSPersistentContainer
-             .defaultDirectoryURL()
-             .appendingPathComponent("feed-store.sqlite")
-    
     private lazy var httpClient: HTTPClient = {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
     private lazy var store: FeedStore = {
-        try! CoreDataFeedStore(storeURL: localStoreURL)
+        try! CoreDataFeedStore(
+            storeURL: NSPersistentContainer
+                .defaultDirectoryURL()
+                .appendingPathComponent("feed-store.sqlite"))
     }()
     
     convenience init(httpClient: HTTPClient, store: FeedStore) {
@@ -41,8 +40,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func configureWindow() {
         let remoteURL = URL(string: "https://api.jsonbin.io/v3/b/63c6b96a01a72b59f24d25b7?meta=false")!
         
-        let remoteClient = makeRemoteClient()
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
         
         let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
         
@@ -55,9 +53,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: localFeedLoader))
         )
         window?.makeKeyAndVisible()
-    }
-    
-    func makeRemoteClient() -> HTTPClient {
-        return httpClient
     }
 }
