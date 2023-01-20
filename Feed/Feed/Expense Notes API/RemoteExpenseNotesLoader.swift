173ed4164cc9f1d123119c140519d913d131abff
m00nbek <melikulov990@gmail.com>
2023-01-20 18:38:07 +0500

    Replace RemoteExpenseNotesLoader with generic RemoteLoader to remote duplication

--- Feed/Feed/Expense Notes API/RemoteExpenseNotesLoader.swift
+++ Feed/Feed/Expense Notes API/RemoteExpenseNotesLoader.swift
@@ -7,41 +7,10 @@
 
 import Foundation
 
-public final class RemoteExpenseNotesLoader {
-    private let url: URL
-    private let client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = Swift.Result<[ExpenseNote], Swift.Error>
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteExpenseNotesLoader.map(data, from: response))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try ExpenseNotesMapper.map(data, from: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
+public typealias RemoteExpenseNotesLoader = RemoteLoader<[ExpenseNote]>
+
+public extension RemoteExpenseNotesLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: ExpenseNotesMapper.map)
     }
 }
