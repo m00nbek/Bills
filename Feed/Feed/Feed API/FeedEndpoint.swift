9dc8954d168176b4e312f132853ec28f9b5f0df4
m00nbek <melikulov990@gmail.com>
2023-01-24 11:49:33 +0500

    Add feed endpoint with "after_id" param

--- Feed/Feed/Feed API/FeedEndpoint.swift
+++ Feed/Feed/Feed API/FeedEndpoint.swift
@@ -8,18 +8,20 @@
 import Foundation
 
 public enum FeedEndpoint {
-    case get
+    case get(after: FeedExpense? = nil)
     
     public func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
+            
+        case let .get(expense):
             var components = URLComponents()
             components.scheme = baseURL.scheme
             components.host = baseURL.host
             components.path = baseURL.path + "/v1/feed"
             components.queryItems = [
-                URLQueryItem(name: "limit", value: "10")
-            ]
+                URLQueryItem(name: "limit", value: "10"),
+                expense.map { URLQueryItem(name: "after_id", value: $0.id.uuidString) },
+            ].compactMap { $0 }
             return components.url!
         }
     }
