d09114b549f89d5fa05bfc03c60460f4723df5bd
m00nbek <melikulov990@gmail.com>
2023-01-24 11:38:55 +0500

    Load more on error tap 

--- App/AppTests/FeedUIIntegrationTests.swift
+++ App/AppTests/FeedUIIntegrationTests.swift
@@ -220,6 +220,22 @@ class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.loadMoreFeedErrorMessage, nil)
     }
     
+    func test_tapOnLoadMoreErrorView_loadsMore() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+
+        sut.simulateLoadMoreFeedAction()
+        XCTAssertEqual(loader.loadMoreCallCount, 1)
+
+        sut.simulateTapOnLoadMoreFeedError()
+        XCTAssertEqual(loader.loadMoreCallCount, 1)
+
+        loader.completeLoadMoreWithError()
+        sut.simulateTapOnLoadMoreFeedError()
+        XCTAssertEqual(loader.loadMoreCallCount, 2)
+    }
+    
     func test_expenseSelection_notifiesHandler() {
         let expense0 = makeExpense()
         let expense1 = makeExpense()
