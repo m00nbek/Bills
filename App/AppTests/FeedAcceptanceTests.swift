d3db50243d236fd33f1354452d35f32c1ace786e
m00nbek <melikulov990@gmail.com>
2023-01-28 18:15:19 +0500

    update url path in `FeedAcceptanceTests`

--- App/AppTests/FeedAcceptanceTests.swift
+++ App/AppTests/FeedAcceptanceTests.swift
@@ -105,16 +105,16 @@ class FeedAcceptanceTests: XCTestCase {
     
     private func makeData(for url: URL) -> Data {
         switch url.path {
-        case "/v1/feed" where url.query?.contains("after_id") == false:
+        case "/bills/v1/feed" where url.query?.contains("after_id") == false:
             return makeFirstFeedPageData()
 
-        case "/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
+        case "/bills/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
             return makeSecondFeedPageData()
 
-        case "/v1/feed" where url.query?.contains("after_id=166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F") == true:
+        case "/bills/v1/feed" where url.query?.contains("after_id=166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F") == true:
             return makeLastEmptyFeedPageData()
 
-        case "/v1/expense/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/notes":
+        case "/bills/v1/expense/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/notes":
             return makeNotesData()
 
         default:
@@ -127,14 +127,14 @@ class FeedAcceptanceTests: XCTestCase {
             [
                 "id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086",
                 "title": "any title",
-                "timestamp": Date().ISO8601Format(),
+                "created_at": Date().ISO8601Format(),
                 "cost": 0,
                 "currency": "USD"
             ],
             [
                 "id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A",
                 "title": "any title",
-                "timestamp": Date().ISO8601Format(),
+                "created_at": Date().ISO8601Format(),
                 "cost": 0,
                 "currency": "USD"
             ]
@@ -146,7 +146,7 @@ class FeedAcceptanceTests: XCTestCase {
             [
                 "id": "166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F",
                 "title": "any title",
-                "timestamp": Date().ISO8601Format(),
+                "created_at": Date().ISO8601Format(),
                 "cost": 0,
                 "currency": "USD"
             ],
